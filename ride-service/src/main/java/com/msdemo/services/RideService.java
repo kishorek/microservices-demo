6e510088763cc68281f45532ecb67a74cc450385
kishorek <libcomet@gmail.com>
2023-01-17 19:04:27 -0500

    added services

--- ride-service/src/main/java/com/msdemo/services/RideService.java
+++ ride-service/src/main/java/com/msdemo/services/RideService.java
@@ -1,8 +1,6 @@
 package com.msdemo.services;
 
-import com.msdemo.rideservice.grpc.RideSearchRequest;
-import com.msdemo.rideservice.grpc.RideSearchResponse;
-import com.msdemo.rideservice.grpc.RideServiceGrpc;
+import com.msdemo.rideservice.grpc.*;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
@@ -11,5 +9,10 @@ public class RideService extends RideServiceGrpc.RideServiceImplBase {
     @Override
     public void findRides(RideSearchRequest request, StreamObserver<RideSearchResponse> responseObserver) {
         super.findRides(request, responseObserver);
+    }
+
+    @Override
+    public void postRide(RideRequest request, StreamObserver<RideResponse> responseObserver) {
+        super.postRide(request, responseObserver);
     }
 }
