973c8caa23a73b2f916145a492aba88545e81bae
kishorek <libcomet@gmail.com>
2023-01-18 19:58:11 -0500

    updated dependencies

--- ride-service/src/main/java/com/msdemo/Main.java
+++ ride-service/src/main/java/com/msdemo/Main.java
@@ -8,4 +8,18 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    /*@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("All beans from the context:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }*/
 }
