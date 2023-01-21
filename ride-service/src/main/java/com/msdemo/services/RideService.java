04a4e6baa1d2fa64d061fed0bcde002d1e38cbf4
kishorek <libcomet@gmail.com>
2023-01-21 10:25:53 -0500

    added rides list api

--- ride-service/src/main/java/com/msdemo/services/RideService.java
+++ ride-service/src/main/java/com/msdemo/services/RideService.java
@@ -1,10 +1,13 @@
 package com.msdemo.services;
 
+import com.msdemo.models.Ride;
 import com.msdemo.repositories.RideRepository;
 import com.msdemo.rideservice.grpc.*;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @GrpcService
 public class RideService extends RideServiceGrpc.RideServiceImplBase {
@@ -14,7 +17,13 @@ public class RideService extends RideServiceGrpc.RideServiceImplBase {
 
     @Override
     public void findRides(RideSearchRequest request, StreamObserver<RideSearchResponse> responseObserver) {
-        
+        List<Ride> rides = this.repository.findByFromAndTo(request.getFrom(),request.getTo());
+        RideSearchResponse.Builder response = RideSearchResponse.newBuilder();
+        for(Ride ride:rides){
+            response.addRides(RideResponse.newBuilder().setFrom(ride.getFrom()).setTo(ride.getTo()).setId(ride.getId()).build());
+        }
+        responseObserver.onNext(response.build());
+        responseObserver.onCompleted();
     }
 
     @Override
