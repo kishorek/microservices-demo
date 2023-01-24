4782dd6470084b390fc6b7b6fe2d47139899322f
kishorek <libcomet@gmail.com>
2023-01-23 19:54:28 -0500

    fixed db connection

--- ride-service/src/main/java/com/msdemo/Main.java
+++ ride-service/src/main/java/com/msdemo/Main.java
@@ -1,10 +1,19 @@
 package com.msdemo;
 
 import com.msdemo.repositories.RideRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Main {
@@ -12,11 +21,31 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    private void probeDB() throws SQLException {
+        System.out.println(dataSource.getConnection().getMetaData().getURL());
+
+        DatabaseMetaData metaData = dataSource.getConnection().getMetaData();
+        ResultSet tables = metaData.getTables(null, null, null, new String[] { "TABLE" });
+        while (tables.next()) {
+            String tableName=tables.getString("TABLE_NAME");
+            System.out.println(tableName);
+            ResultSet columns = metaData.getColumns(null,  null,  tableName, "%");
+            while (columns.next()) {
+                String columnName=columns.getString("COLUMN_NAME");
+                System.out.println("\t" + columnName);
+            }
+        }
+    }
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
-    public CommandLineRunner checkDB(RideRepository repository){
+    public CommandLineRunner checkDB(RideRepository repository) {
         return (args) -> {
+//            this.probeDB();
             System.out.println(repository.findAll());
-            System.out.println(repository.findByFromAndTo("Chennai","Pondicherry"));
+            System.out.println(repository.findByFromAndTo("Chennai", "Pondicherry"));
         };
     }
 }
